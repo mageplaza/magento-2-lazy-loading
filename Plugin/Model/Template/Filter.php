006e3d397777034711d609ee59690e498593d33d
prime <trungbd@mageplaza.com>
2019-10-24 11:12:47 +0700

    placeholder cms

--- Plugin/Model/Template/Filter.php
+++ Plugin/Model/Template/Filter.php
@@ -23,15 +23,24 @@ namespace Mageplaza\LazyLoading\Plugin\Model\Template;
 
 use Magento\Cms\Model\Template\Filter as CmsFilter;
 use Mageplaza\LazyLoading\Helper\Data as HelperData;
+use Mageplaza\LazyLoading\Helper\Image as HelperImage;
+use Magento\Framework\Filesystem\Io\File;
 
 class Filter
 {
     protected $helperData;
+    protected $helperImage;
+    protected $file;
+    protected $moveImgTo = 'pub/media/mageplaza/lazyloading/';
 
     public function __construct(
-        HelperData $helperData
+        HelperData $helperData,
+        HelperImage $helperImage,
+        File $file
     ) {
-        $this->helperData = $helperData;
+        $this->helperData  = $helperData;
+        $this->helperImage = $helperImage;
+        $this->file        = $file;
     }
 
     public function afterFilter(CmsFilter $filter, $result)
@@ -40,15 +49,16 @@ class Filter
             return $result;
         }
 
+        $placeHolder = '';
+        $holderType  = '';
+
         if ($this->helperData->getLoadingType() === 'icon') {
-            $class       = 'mplazyload-icon';
+            $class       = 'mplazyload-icon mplazyload-cms';
             $placeHolder = $this->helperData->getIcon();
         } else {
             $holderType = $this->helperData->getPlaceholderType();
             $class      = 'mplazyload-' . $this->helperData->getPlaceholderType();
-            if ($holderType !== 'transparent') {
-                $placeHolder = 'test';
-            } else {
+            if ($holderType === 'transparent') {
                 $placeHolder = HelperData::DEFAULT_IMAGE;
             }
         }
@@ -58,8 +68,17 @@ class Filter
         $search   = [];
         foreach ($matches[0] as $img) {
             if ($img) {
+                if ($holderType !== 'transparent') {
+                    $imgSrc  = $this->getImageSrc($img);
+                    $imgPath = substr($imgSrc, strpos($imgSrc, 'pub'));
+                    $imgInfo = $this->file->getPathInfo($imgPath);
+                    $this->optimizeImage($imgPath, $imgInfo);
+                    $placeHolder = $this->helperImage->getBaseMediaUrl()
+                        . '/mageplaza/lazyloading/'
+                        . $imgInfo['basename'];
+                }
                 $strProcess   = preg_replace('/src="/', 'data-src="', $img);
-                $replaceClass = '<img class="mplazyload mplazyload-cms ' . $class . '" src="' . $placeHolder . '"';
+                $replaceClass = '<img class="mplazyload ' . $class . '" src="' . $placeHolder . '"';
                 $strProcess   = preg_replace('/<img/', $replaceClass, $strProcess);
                 $replaced[]   = $strProcess;
                 $search[]     = $img;
@@ -67,5 +86,70 @@ class Filter
         }
 
         return str_replace($search, $replaced, $result);
+    }
+
+    public function getImageSrc($img)
+    {
+        preg_match('/src\s*=\s*"(.+?)"/', $img, $matches);
+
+        return $matches[1];
+    }
+
+    public function optimizeImage($imgPath, $imgInfo)
+    {
+        $quality = 10;
+
+        if ($dir = opendir($imgInfo['dirname'])) {
+            $checkValidImage = getimagesize($imgPath);
+
+            if ($checkValidImage) {
+                $this->changeQuality($imgPath, $this->moveImgTo . $imgInfo['basename'], $quality);
+            }
+            closedir($dir);
+        }
+    }
+
+    public function changeQuality($srcImage, $destImage, $imageQuality)
+    {
+        list($width, $height, $type) = getimagesize($srcImage);
+        $newCanvas = imagecreatetruecolor($width, $height);
+        switch (strtolower(image_type_to_mime_type($type))) {
+            case 'image/jpeg':
+                $newImage = imagecreatefromjpeg($srcImage);
+                break;
+            case 'image/JPEG':
+                $newImage = imagecreatefromjpeg($srcImage);
+                break;
+            case 'image/png':
+                $newImage = imagecreatefrompng($srcImage);
+                break;
+            case 'image/PNG':
+                $newImage = imagecreatefrompng($srcImage);
+                break;
+            case 'image/gif':
+                $newImage = imagecreatefromgif($srcImage);
+                break;
+            default:
+                return false;
+        }
+
+        if (imagecopyresampled(
+            $newCanvas,
+            $newImage,
+            0,
+            0,
+            0,
+            0,
+            $width,
+            $height,
+            $width,
+            $height
+        )) {
+            if (imagejpeg($newCanvas, $destImage, $imageQuality)) {
+                imagedestroy($newCanvas);
+
+                return true;
+            }
+        }
     }
 }
